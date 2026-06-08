3cb0af51eaa06cb5b2a6ab386a2603ac4337f0ff
Rodolfin05 <rodolfosouzaparreiras@gmail.com>
2026-06-08 00:56:19 -0300

    Migra para PostgreSQL e configura o Docker

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6g6t@k3xr$@3+$a(0^s-rfi0-md!4@+5k_+si)q4*v5k=%ax_$'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-sgta-dev-key-troque-em-producao')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  
+        'NAME': config('DB_NAME'),                 
+        'USER': config('DB_USER'),                 
+        'PASSWORD': config('DB_PASSWORD'),         
+        'HOST': config('DB_HOST', default='db'),    
+        'PORT': config('DB_PORT', default='5432'), 
     }
 }
 
